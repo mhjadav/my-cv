fb4966f2fda73e6ddf07f5ed3f1858c15873f1e5
Mahipat Jadav <mj@mojaave.com>
2023-02-03 19:38:38 +0530

    Only index

--- src/components/profile-pic.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Img from 'gatsby-image';
-
-const ProfilePic = ({ profileImage, name }) => (<Img className="card-img-top" fluid={profileImage.childImageSharp.fluid} alt={name} />)
-
-
-export default ProfilePic;
